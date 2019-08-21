8bf212d35680a02bce80a3899ab2a93ee03f6e9f
juniorze <junior.ze.jw@gmail.com>
2019-08-21 11:08:03 -0300

    Adiciona port como variavel env e now.json para integração com ZEIT

--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser");
 const sqlite = require("sqlite");
 const dbConnection = sqlite.open("banco.sqlite", { Promise });
 
+const port = process.env.PORT || 3333;
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -82,7 +84,7 @@ const init = async () => {
   );
 };
 init();
-app.listen(3333, err => {
+app.listen(port, err => {
   if (err) {
     console.log("Não foi possivel iniciar a aplicação");
   } else {
